6e161b7518a658d9003ad825525316716f9c9a95
Jeremie Ges <bonjour@gesjeremie.fr>
2015-12-10 12:43:54 +0200

    clean indent validations

--- website/app/validations.php
+++ website/app/validations.php
@@ -5,19 +5,12 @@
  */
 Validator::extend('match_password', function($attribute, $value, $parameters)
 {
-
 	if (Hash::check($value, Auth::user()->password))
-	{
-
-		return true;
-
-    }
-    else
-    {
-
-    	
-    	return false;
-
-    }
-
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 });
