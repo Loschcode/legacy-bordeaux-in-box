3213d24140e4a28b677aaa05ab4be07dd2585d00
Jeremie Ges <bonjour@gesjeremie.fr>
2015-12-21 11:10:48 +0100

    fix typo lib boxes questions

--- website/resources/views/admin/boxes/questions/edit.blade.php
+++ website/resources/views/admin/boxes/questions/edit.blade.php
@@ -72,7 +72,7 @@
   <!-- Type -->
   <div class="form-group @if ($errors->first('type')) has-error has-feedback @endif">
     {!! Form::label("type", "Type", ['class' => 'control-label']) !!}
-    {!! Form::select('type', Form::getPossibleQuestionTypes(), (Request::old("type")) ? Request::old("type") : $question->type, ['class' => 'form-control']) !!}<br />
+    {!! Form::select('type', Html::getPossibleQuestionTypes(), (Request::old("type")) ? Request::old("type") : $question->type, ['class' => 'form-control']) !!}<br />
     @if ($errors->first('type'))
       <span class="glyphicon glyphicon-remove form-control-feedback"></span>
       <span class="help-block">{{ $errors->first('type') }}</span>
