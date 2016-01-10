cc14976af6e89ca6f73abb6a2598380852a627ae
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-10 21:48:16 +0100

    fix bug gridle .grid-centered

--- website/resources/views/sandbox/ui/pages/home.blade.php
+++ website/resources/views/sandbox/ui/pages/home.blade.php
@@ -2,11 +2,12 @@
 @section('content')
   <br/><br/>
   <div class="container">
-    <p>Ceci est un test avec une font size de base</p>
-    <div class="alert --blue">
-      <strong>Bravo !</strong> Tu deviens un connard de base, génial :p
-
+    <div class="grid-6 grid-centered">
+      <div class="alert --blue">
+        <strong>Bravo !</strong> Tu deviens un connard de base, génial :p
+      </div>
     </div>
+
     <div class="alert --blue --xs">Default alert</div>
     <div class="alert --blue --sm">Default alert</div>
     <div class="alert --blue --md">Default alert</div>
