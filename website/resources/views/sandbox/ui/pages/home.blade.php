80c765bbee3921951d4c22f6ee520de8536884bd
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-10 21:22:34 +0100

    alert component

--- website/resources/views/sandbox/ui/pages/home.blade.php
+++ website/resources/views/sandbox/ui/pages/home.blade.php
@@ -2,10 +2,18 @@
 @section('content')
   <br/><br/>
   <div class="container">
-    <h1>Comment ça marche ?</h1>
-    <p>Des petites marques locales, des créateurs, des artisans de Gironde ... Cela dépend des mois et des envies de l'équipe ! Chaque mois est différent donc il y aura toujours quelque chose pour te plaire dedans. Pour te donner quelques exemples, on y a déjà glissé du vin, des bijoux, du parfum, des cosmétiques, des cartes cadeaux pour des séances photo, des petites plantes, des préparations pour canelés, des places de concert, des dessins d'artistes ou encore des jouets pour enfants (pour les Mamoune) !
+    <p>Ceci est un test avec une font size de base</p>
+    <div class="alert --blue">
+      <strong>Bravo !</strong> Tu deviens un connard de base, génial :p
 
-Si tu es très très curieuse, voici le lien vers nos albums photo facebook où nous réunissons les photos prises par vous !</p>
+    </div>
+    <div class="alert --blue --xs">Default alert</div>
+    <div class="alert --blue --sm">Default alert</div>
+    <div class="alert --blue --md">Default alert</div>
+    <div class="alert --blue --lg">Default alert</div>
+    <div class="alert --blue --xl">Default alert</div>
+
+
   </div>
 
 @stop
