981790a279716bc45cf62a891c2c13084a72985a
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-10 20:24:34 +0100

    mixins font family

--- website/resources/views/sandbox/ui/pages/home.blade.php
+++ website/resources/views/sandbox/ui/pages/home.blade.php
@@ -2,7 +2,10 @@
 @section('content')
   <br/><br/>
   <div class="container">
-    <div class="alert"><p>Test alert</p></div>
+    <h1>Comment ça marche ?</h1>
+    <p>Des petites marques locales, des créateurs, des artisans de Gironde ... Cela dépend des mois et des envies de l'équipe ! Chaque mois est différent donc il y aura toujours quelque chose pour te plaire dedans. Pour te donner quelques exemples, on y a déjà glissé du vin, des bijoux, du parfum, des cosmétiques, des cartes cadeaux pour des séances photo, des petites plantes, des préparations pour canelés, des places de concert, des dessins d'artistes ou encore des jouets pour enfants (pour les Mamoune) !
+
+Si tu es très très curieuse, voici le lien vers nos albums photo facebook où nous réunissons les photos prises par vous !</p>
   </div>
 
 @stop
