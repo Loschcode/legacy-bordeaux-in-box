18fa3b9557b5aeffc70f07873d241320bf738561
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-10 22:52:28 +0100

    badge component

--- website/resources/views/sandbox/ui/pages/home.blade.php
+++ website/resources/views/sandbox/ui/pages/home.blade.php
@@ -3,16 +3,14 @@
   <br/><br/>
   <div class="container">
     <div class="grid-6 grid-centered">
-      <div class="alert --blue">
-        <strong>Bravo !</strong> Tu deviens un connard de base, génial :p
-      </div>
-    </div>
+      <div class="alert">Test normal</div>
+      <span class="badge">1 nouveau message</span>
+      <span class="badge --number --pink">100</span>
+      <span class="badge --red">+3</span>
+      <span class="badge --orange">Nouveau</span>
+      <span class="badge --green">Nouveau</span>
 
-    <div class="alert --blue --xs">Default alert</div>
-    <div class="alert --blue --sm">Default alert</div>
-    <div class="alert --blue --md">Default alert</div>
-    <div class="alert --blue --lg">Default alert</div>
-    <div class="alert --blue --xl">Default alert</div>
+    </div>
 
 
   </div>
