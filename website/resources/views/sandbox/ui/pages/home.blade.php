2f6b8ae49b25374e9e2c1a5b0626c1533bb18a25
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-10 17:36:43 +0100

    create the color palette, the sass functions color related: color,  color-light, color-dark

--- website/resources/views/sandbox/ui/pages/home.blade.php
+++ website/resources/views/sandbox/ui/pages/home.blade.php
@@ -4,4 +4,15 @@
   <div class="alert --grey"></div>
   <div class="alert --grey-dark"></div>
 
+  <div class="alert --orange-light"></div>
+  <div class="alert --orange"></div>
+  <div class="alert --orange-dark"></div>
+
+  <div class="alert --green-light"></div>
+  <div class="alert --green"></div>
+  <div class="alert --green-dark"></div>
+
+  <div class="alert --purple-light"></div>
+  <div class="alert --purple"></div>
+  <div class="alert --purple-dark"></div>
 @stop
