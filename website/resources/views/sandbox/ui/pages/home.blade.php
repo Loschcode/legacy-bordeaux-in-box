ead1d7800b3af910d1203950f3a0f63e8564f3d3
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-10 19:32:05 +0100

    alert component in progress

--- website/resources/views/sandbox/ui/pages/home.blade.php
+++ website/resources/views/sandbox/ui/pages/home.blade.php
@@ -1,5 +1,8 @@
 @extends('sandbox.layouts.ui')
 @section('content')
-  <div class="test">So what's the solution ?</div>
-  <p>In my recent redesign of my website, I wanted to come up with a really simple system of establishing vertical (and horizontal) rhythms that would work responsively. It turns out the answer is really simple, if you’re using Sass. Here it is in all its glory</p>
+  <br/><br/>
+  <div class="container">
+    <div class="alert"><p>Test alert</p></div>
+  </div>
+
 @stop
