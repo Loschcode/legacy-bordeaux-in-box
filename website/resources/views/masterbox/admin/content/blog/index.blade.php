a377f1b8e708640f083bf5d956a3b23a40e9c9e2
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-22 09:17:41 +0100

    subtitle style

--- website/resources/views/masterbox/admin/content/blog/index.blade.php
+++ website/resources/views/masterbox/admin/content/blog/index.blade.php
@@ -21,6 +21,7 @@
   <div class="row">
     <div class="grid-8">
       <h1 class="title title__section">Blog</h1>
+      <h2 class="title title__subsection">Les articles</h2>
     </div>
     <div class="grid-4">
       <div class="+text-right">
