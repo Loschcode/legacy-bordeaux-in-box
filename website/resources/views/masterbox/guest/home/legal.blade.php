f50c8c918b2eed69d2abab5a61e945c0410a447d
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-23 16:01:32 +0100

    legal

--- website/resources/views/masterbox/guest/home/legal.blade.php
+++ website/resources/views/masterbox/guest/home/legal.blade.php
@@ -2,15 +2,20 @@
 
 @section('content')
 
+<div class="section section__wrapper">
+  <h1 class="section__title --page">Mentions Légales</h1>
+</div>
 
 <div class="page page__wrapper">
   <div class="container">
     <div class="grid-10 grid-centered">
-      <div class="typography +text-center">
+      <div class="typography">
         {!! Markdown::convertToHtml($legal->content) !!}
       </div>
     </div>
   </div>
 </div>
+
+<div class="+spacer"></div>
 
 @stop
