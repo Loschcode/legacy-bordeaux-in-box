fe1727bae440dc59a222ea14eef14f3e6c26f1e4
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-23 15:30:53 +0100

    subscribe page

--- website/resources/views/masterbox/connect/customer/subscribe.blade.php
+++ website/resources/views/masterbox/connect/customer/subscribe.blade.php
@@ -6,16 +6,15 @@
   data-form-errors="{{ $errors->has() }}"
 ></div>
 
+<div class="section section__wrapper">
+  <h1 class="section__title --page">Inscription</h1>
+</div>
 <div class="+spacer-small"></div>
 
 <div class="container">
   <div class="grid-8 grid-centered">
-    <div class="panel">
-      <div class="panel__heading">
-        <h2 class="panel__title">Inscription</h2>
-      </div>
-      <div class="panel__content">
-        <p>
+
+        <p class="typography +text-center">
           C’est le moment de créer ton compte, le formulaire juste en dessous va te poser quelques questions histoire de savoir qui tu es ...<br/>
           Si tu as déjà un compte, tu peux <a href="{{ action('MasterBox\Connect\CustomerController@getLogin') }}">te connecter</a> !
         </p>
@@ -25,56 +24,40 @@
         <div class="+spacer-extra-small"></div>
           
           <div class="form">
-            
-            <h4 class="form__title">Informations personnelles</h4>
-            <div class="row">
-              <div class="grid-6 no-gutter">
-                {!! Form::text("first_name", Request::old("first_name"), ['placeholder' => 'Prénom', 'class' => 'form__input --grouped-left']) !!}
+                            {!! Form::text("first_name", Request::old("first_name"), ['placeholder' => 'Prénom', 'class' => 'form__input']) !!}
                 {!! Html::checkError('first_name', $errors) !!}
-              </div>
-              <div class="grid-6 no-gutter">
-                {!! Form::text("last_name", Request::old("last_name"), ['placeholder' => 'Nom de famille', 'class' => 'form__input --grouped-right']) !!}
+                            <div class="+spacer-extra-small"></div>
+
+                {!! Form::text("last_name", Request::old("last_name"), ['placeholder' => 'Nom de famille', 'class' => 'form__input']) !!}
                 {!! Html::checkError('last_name', $errors) !!}
-              </div>
-            </div>
+     
             
-            <div class="+spacer-small"></div>
+            <div class="+spacer-extra-small"></div>
             
-            <h4 class="form__title">Contact</h4>
 
-            <div class="row">
-              <div class="grid-6 no-gutter">
-                {!! Form::text("email", Request::old("email"), ['placeholder' => 'Email', 'class' => 'form__input --grouped-left']) !!}
+
+                {!! Form::text("email", Request::old("email"), ['placeholder' => 'Email', 'class' => 'form__input']) !!}
                 {!! Html::checkError('email', $errors) !!}
-              </div>
-              <div class="grid-6 no-gutter">
-                {!! Form::text("phone", Request::old("phone"), ['placeholder' => 'Téléphone', 'class' => 'form__input --grouped-right']) !!}
+            <div class="+spacer-extra-small"></div>
+
+                {!! Form::text("phone", Request::old("phone"), ['placeholder' => 'Téléphone', 'class' => 'form__input']) !!}
                 {!! Html::checkError('phone', $errors) !!}
-              </div>
-            </div>
             
-            <div class="+spacer-small"></div>
+            <div class="+spacer-extra-small"></div>
 
-            <h4 class="form__title">Sécurité</h4>
 
-            <div class="row">
-              <div class="grid-6 no-gutter">
-                {!! Form::password("password", ['placeholder' => 'Mot de passe', 'class' => 'form__input --grouped-left']) !!}
+                {!! Form::password("password", ['placeholder' => 'Mot de passe', 'class' => 'form__input']) !!}
                 {!! Html::checkError('password', $errors) !!}
-              </div>
-              <div class="grid-6 no-gutter">
-                {!! Form::password("password_confirmation", ['placeholder' => 'Confirmation du mot de passe', 'class' => 'form__input --grouped-right']) !!}
+                 <div class="+spacer-extra-small"></div>
+
+
+                {!! Form::password("password_confirmation", ['placeholder' => 'Confirmation du mot de passe', 'class' => 'form__input']) !!}
                 {!! Html::checkError('password_confirmation', $errors) !!}
-              </div>
-            </div>
+                <div class="+spacer-small"></div>
 
 
 
-          </div>
-      </div>
-      <div class="panel__footer">
-        <button type="submit" class="button button__submit --panel">M'inscrire</button>
-      </div>
+        <button type="submit" class="button button__submit">M'inscrire</button>
     </div>
     {!! Form::close() !!}
 
