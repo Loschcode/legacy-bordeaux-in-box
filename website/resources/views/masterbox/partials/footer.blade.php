a5c0aaf5c86c58ca680b2f0b17f1764baa5b3b3c
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-23 13:17:14 +0100

    add links social networks

--- website/resources/views/masterbox/partials/footer.blade.php
+++ website/resources/views/masterbox/partials/footer.blade.php
@@ -3,11 +3,18 @@
     <div class="row">
       <div class="grid-4">
         <ul>
+          <li><a target="_blank" href="https://www.facebook.com/BordeauxinBox/"><i class="fa fa-facebook-official"></i> Facebook</a></li>
+          <li><a target="_blank" href="https://www.instagram.com/bordeauxinbox/"><i class="fa fa-instagram"></i> Instagram</a></li>
+          <li></li>
+        </ul>
+      </div>
+      <div class="grid-4">
+        <ul>
           <li><a href="{{ action('MasterBox\Guest\HomeController@getHelp') }}">Foire aux questions</a></li>
           <li><a href="{{ action('MasterBox\Guest\ContactController@getIndex') }}">Nous contacter</a></li>
         </ul>
       </div>
-      <div class="grid-4 push-4">
+      <div class="grid-4">
         <ul>
         <li><a href="{{ action('MasterBox\Guest\HomeController@getLegals') }}">Mentions Légales</a></li>
         <li><a href="{{ action('MasterBox\Guest\HomeController@getCgv') }}">Conditions générales de vente</a></li>
