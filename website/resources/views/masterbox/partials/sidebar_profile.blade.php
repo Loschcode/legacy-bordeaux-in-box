f64354e5e2015692a967941c87c5cf6451bd60f7
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-26 16:41:10 +0100

    links sidebar

--- website/resources/views/masterbox/partials/sidebar_profile.blade.php
+++ website/resources/views/masterbox/partials/sidebar_profile.blade.php
@@ -2,7 +2,7 @@
   <ul class="sidebar__list">
     <li class="sidebar__item"><a class="sidebar__link {{ Html::cssLinkProfileMenuActive('account', $active_menu) }}" href="{{ action('MasterBox\Customer\ProfileController@getIndex') }}">Mon compte</a></li>
     <li class="sidebar__item"><a class="sidebar__link {{ Html::cssLinkProfileMenuActive('orders', $active_menu) }}" href="{{ action('MasterBox\Customer\ProfileController@getOrders') }}">Abonnements</a></li>
-    <li class="sidebar__item"><a class="sidebar__link" href="#">Contact</a></li>
-    <li class="sidebar__item --last"><a class="sidebar__link" href="#">Déconnexion</a></li>
+    <li class="sidebar__item"><a class="sidebar__link" href="{{ action('MasterBox\Guest\ContactController@getIndex') }}">Contact</a></li>
+    <li class="sidebar__item --last"><a class="sidebar__link" href="{{ action('MasterBox\Connect\CustomerController@getLogout') }}">Déconnexion</a></li>
   </ul>
 </div>
