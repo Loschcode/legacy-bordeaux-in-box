2d2206ee27d3cbaef68ad2e0ce2580fc8e124ab3
Jeremie Ges <ges.jeremie@gmail.com>
2016-01-30 17:07:40 +0100

    changed something in billing address

--- website/resources/views/masterbox/customer/order/billing_address.blade.php
+++ website/resources/views/masterbox/customer/order/billing_address.blade.php
@@ -49,12 +49,65 @@
         <div class="grid-8 grid-centered">
             
           {{-- Delivery informations --}}
-          @if ($order_preference->isGift())
-            @include('masterbox.partials.gift_destination')
-          @else
-            @include('masterbox.partials.classic_destination')
-          @endif
-          
+
+          <div class="panel">
+            <div class="panel__heading">
+              <h2 class="panel__title">Informations de livraison</h2>
+            </div>
+            <div class="panel__content --white">
+
+
+              <div class="row billing__container">
+                <div class="grid-6">
+                  
+                  <div class="billing__label">
+                    {!! Form::label('destination_first_name', 'Prénom') !!}
+                  </div>
+                  
+                  {!! Form::text("destination_first_name", ($order_building->destination_first_name) ? $order_building->destination_first_name : Request::old("destination_first_name"), ['class' => 'billing__input']) !!}
+                  {!! Html::checkError('destination_first_name', $errors) !!}
+
+                </div>
+                <div class="grid-6">
+                  <div class="billing__label">
+                    {!! Form::label('destination_last_name', 'Nom de famille') !!}
+                  </div>
+
+                  {!! Form::text("destination_last_name", ($order_building->destination_last_name) ? $order_building->destination_last_name : Request::old("destination_last_name"), ['class' => 'billing__input']) !!}
+
+                  {!! Html::checkError('destination_last_name', $errors) !!}
+
+                </div>
+              </div>
+
+              <div class="row billing__container">
+                <div class="grid-6">
+                  
+                  <div class="billing__label">
+                    {!! Form::label('destination_city', 'Ville') !!}
+                  </div>
+                  
+                  {!! Form::text("destination_city", ($order_building->destination_city) ? $order_building->destination_city : Request::old("destination_city"), ['class' => 'billing__input']) !!}
+                  {!! Html::checkError('destination_city', $errors) !!}
+
+
+                </div>
+                <div class="grid-6">
+                  <div class="billing__label">
+                    {!! Form::label('destination_zip', 'Code postal') !!}
+                  </div>
+                  {!! Form::text("destination_zip", ($order_building->destination_zip) ? $order_building->destination_zip : Request::old("destination_zip"), ['class' => 'billing__input']) !!}
+                  {!! Html::checkError('destination_zip', $errors) !!}
+                </div>
+              </div>
+
+              <div class="billing__label">
+                {!! Form::label('destination_address', 'Adresse') !!}
+              </div>
+              {!! Form::textarea("destination_address", ($order_building->destination_address) ? $order_building->destination_address : Request::old("destination_address"), ['class' => 'billing__input --textarea']) !!}
+              {!! Html::checkError('destination_address', $errors) !!}
+            </div>
+          </div>
           <div class="+spacer-small"></div>
           
           <!-- If the user already filled an address and it's not his first order -->
