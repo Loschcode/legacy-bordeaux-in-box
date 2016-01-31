ab12c54b1de323182f94a59a460ec26c7b0bb42f
Loschcode <bonjour@laurentschaffner.com>
2016-01-31 12:21:07 +0100

    progress geocoding

--- website/app/Libraries/GoogleGeocoding.php
+++ website/app/Libraries/GoogleGeocoding.php
@@ -14,17 +14,40 @@ class GoogleGeocoding {
     public static function getCoordinates($address, $city, $zip)
     {
 
-      $key = Config::get('services.google.geocoding.key');
-
       $full_address = urlencode("$address, $city $zip");
+    
+      /**
+       * If it didn't work
+       */
+      if (!$result = self::get_datas($full_address))
+        return ['success' => FALSE, 'error' => 'Impossible to retrieve datas'];
+
+      /**
+       * If it worked we save it and return it
+       * @var [type]
+       */
+      $location = $result->geometry->location;
+
+      return [
+
+              'success' => TRUE,
+              'latitude' => $location->lat,
+              'longitude' => $location->lng,
+
+              ];
+
+    }
+
+    public static function get_datas($full_address)
+    {
+
+      $key = Config::get('services.google.geocoding.key');
 
       $url = "https://maps.googleapis.com/maps/api/geocode/json?address=$full_address&key=$key";
       $input = @file_get_contents($url);
       $datas = json_decode($input);
 
-      dd($url);
-
-      if (isset($datas->result)) return $datas;
+      if (isset($datas->results[0])) return $datas->results[0];
       else return FALSE;
 
     }
