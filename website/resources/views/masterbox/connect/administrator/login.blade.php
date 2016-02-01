b304a5d40bbb0fb4a58868932e846cd948c475c0
Jeremie Ges <ges.jeremie@gmail.com>
2016-02-01 20:57:25 +0100

    admin login page

--- website/resources/views/masterbox/connect/administrator/login.blade.php
+++ website/resources/views/masterbox/connect/administrator/login.blade.php
@@ -1,44 +1,27 @@
-@extends('masterbox.layouts.admin')
+@extends('masterbox.layouts.admin_login')
+
+@section('gotham')
+  {!! Html::gotham([
+    'form-errors-text' => 'Les identifiants sont invalides'
+  ]) !!}
+@stop
 
 @section('content')
   
-  <div id="js-page-login"></div>
-
-  <div class="container">
-
-
-    <div class="spacer50"></div>
-
-    <div class="col-md-4 col-md-offset-4">
-
-      @if (session()->has('message'))
-        <div class="spyro-alert spyro-alert-green">{{ session()->get('message') }}</div>
-      @endif
-
+  <div class="grid-3 grid-centered">
+    <div class="connect connect__wrapper">
+      <div class="connect__thumbnail-wrapper">
+        <img class="connect__thumbnail" src="{{ url('images/macaron-masterbox.png') }}" />
+      </div>
       {!! Form::open() !!}
-
-
-
-      {!! Form::text("email", Request::old("email"), ['placeholder' => 'Email']) !!}
-      @if ($errors->first('email'))
-        <span class="error"><i class="fa fa-times"></i> {{{ $errors->first('email') }}}</span>
-      @endif
-
-      {!! Form::password("password", ['placeholder' => 'Mot de passe']) !!}
-      @if ($errors->first('password'))
-        <span class="error"><i class="fa fa-times"></i> {{{ $errors->first('password') }}}</span>
-      @endif
-
-      {!! Form::submit("Se connecter", ['class' => 'spyro-btn spyro-btn-red spyro-btn-block spyro-btn-lg upper']) !!}
-
-      <div class="spacer20"></div>
-
+      {!! Form::text("email", Request::old("email"), ['placeholder' => 'Email', 'class' => 'form__input']) !!}
+      <div class="+spacer-extra-small"></div> 
+      {!! Form::password("password", ['placeholder' => 'Mot de passe', 'class' => 'form__input']) !!}
+      <div class="+spacer-extra-small"></div>
+      {!! Form::submit("Se connecter", ['class' => 'button button__connect']) !!}
+      {!! Form::close() !!}
     </div>
-
   </div>
 
-  <div class="spacer150"></div>
 
 @stop
-
-?>
---
+++ website/resources/views/masterbox/layouts/admin_login.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+  {{-- Charset --}}
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+  {{-- Title --}}
+  <title>Bordeaux In Box</title>
+
+  {{-- Favicon --}}
+  <link rel="icon" href="{{ url('assets/images/favicon-bib.ico') }}" />
+
+  {{-- Responsive scale --}}
+  <meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0" />
+
+  {{-- FontAwesome (icons) (we use CDN to load the icons faster) --}}
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
+
+  {{-- SASS app --}}
+  <link href="{{ url('stylesheets/vendor.css') }}" rel="stylesheet">
+  <link href="{{ url('stylesheets/masterbox-admin.css') }}" rel="stylesheet">
+
+  {{-- CoffeeScript App --}}
+  @if ( ! $app->environment('production'))
+
+  <script>
+    window.brunch = window.brunch || {};
+    window.brunch.server = 'localhost';
+  </script>
+
+  @endif
+
+  <script src="{{ url('javascripts/vendor.js') }}"></script>
+  <script src="{{ url('javascripts/app.js') }}"></script>
+  <script>require('initialize');</script>
+
+</head>
+<body id="csstyle" class="background-grey" data-environment="{{ app()->environment() }}">
+  
+  @section('gotham')
+    {!! Html::gotham() !!}
+  @show
+
+  <div
+  id="gotham-layout"
+  data-layout="masterbox-admin"
+  ></div>
+  
+  @yield('content')
+
+</body> 
+
+
+</html>
