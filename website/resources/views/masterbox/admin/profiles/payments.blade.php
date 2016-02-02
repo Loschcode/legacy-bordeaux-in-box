b15ea060e8b5f27cdcb07b9e06ae5fc33061d7f8
Jeremie Ges <ges.jeremie@gmail.com>
2016-02-02 15:52:57 +0100

    fix problem space payments

--- website/resources/views/masterbox/admin/profiles/payments.blade.php
+++ website/resources/views/masterbox/admin/profiles/payments.blade.php
@@ -15,9 +15,7 @@
 @stop
 
 @section('content')
-  
-  @include('masterbox.admin.partials.navbar_profiles')
-    
+      
   <div class="row">
     <div class="grid-8">
       <h1 class="title title__section">Abonnement #{{ $profile->id }}</h1>
