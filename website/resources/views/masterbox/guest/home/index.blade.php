b6539eb06339f927663a4cfd568b598db2d51a88
Jeremie Ges <ges.jeremie@gmail.com>
2016-02-04 19:28:40 +0100

    home page responsive

--- website/resources/views/masterbox/guest/home/index.blade.php
+++ website/resources/views/masterbox/guest/home/index.blade.php
@@ -10,53 +10,57 @@
 
 @section('content')
 
-
 <div class="artwork artwork__container">
 	<img class="artwork__picture" src="{{ url('images/artwork.png') }}" />
 </div>
 
-<div class="title --home-punchline">Des surprises tout les mois dans une petite boîte rien que pour toi !</div>
-
-<div class="container">
-	{{-- Buttons to order --}}
-	@if ($next_series->first() !== NULL)
-		@if ($next_series->first()->getCounter() !== 0 || $next_series->first()->getCounter() === FALSE)
-			
-			<div class="row">
-				<div class="grid-3 push-3">
-					<a id="test-pick-gift" class="button button__home-action" href="{{ action('MasterBox\Customer\PurchaseController@getGift') }}"><i class="fa fa-gift"></i>L'offrir</a>
-				</div>
-				<div class="grid-3 push-3">
-					<a id="test-pick-classic" class="button button__home-action" href="{{ action('MasterBox\Customer\PurchaseController@getClassic') }}"><i class="fa fa-shopping-cart"></i> La recevoir</a>
-				</div>
-			</div>
-
-			<div class="counter">
-				<div class="counter__content">
-					Il ne reste que {{$next_series->first()->getCounter()}} box(s) et {{ str_replace('dans', '', strtolower(Html::diffHumans($next_series->first()->delivery, 5
-					))) }} pour commander la box de {!! Html::convertMonth($next_series->first()->delivery) !!}
-				</div>
-			</div>
-
-
-		@endif
-	@endif
-
-	{{-- No more boxes to order --}}
-	@if ($next_series->first() === NULL or $next_series->first()->getCounter() === 0)
-
-		<div class="row">
-			<div class="grid-3 push-3">
-				<a class="button button__home-action js-no-boxes" href="#"><i class="fa fa-gift"></i>L'offrir</a>
-			</div>
-			<div class="grid-3 push-3">
-				<a class="button button__home-action js-no-boxes" href="#"><i class="fa fa-shopping-cart"></i> La recevoir</a>
-			</div>
-		</div>
-	@endif
+<div class="grid-11@xs gr-centered@xs">
+	<div class="title --home-punchline">Des surprises tout les mois dans une petite boîte rien que pour toi !</div>
 </div>
 
 <div class="container">
+
+		{{-- Buttons to order --}}
+		@if ($next_series->first() !== NULL)
+			@if ($next_series->first()->getCounter() !== 0 || $next_series->first()->getCounter() === FALSE)
+				
+				<div class="row row-align-center@xs">
+					<div class="grid-3 push-3 grid-11@xs grid-centered@xs push-0@xs">
+						<a id="test-pick-gift" class="button button__home-action" href="{{ action('MasterBox\Customer\PurchaseController@getGift') }}"><i class="fa fa-gift"></i>L'offrir</a>
+					</div>
+					<div class="grid-3 push-3 grid-11@xs grid-centered@xs push-0@xs">
+						<a id="test-pick-classic" class="button button__home-action" href="{{ action('MasterBox\Customer\PurchaseController@getClassic') }}"><i class="fa fa-shopping-cart"></i> La recevoir</a>
+					</div>
+				</div>
+				
+				<div class="grid-11@xs gr-centered@xs">
+					<div class="counter">
+						<div class="counter__content">
+							Il ne reste que {{$next_series->first()->getCounter()}} box(s) et {{ str_replace('dans', '', strtolower(Html::diffHumans($next_series->first()->delivery, 5
+							))) }} pour commander la box de {!! Html::convertMonth($next_series->first()->delivery) !!}
+						</div>
+					</div>
+				</div>
+
+
+			@endif
+		@endif
+
+		{{-- No more boxes to order --}}
+		@if ($next_series->first() === NULL or $next_series->first()->getCounter() === 0)
+
+			<div class="row">
+				<div class="grid-3 push-3 grid-11@xs grid-centered@xs push-0@xs">
+					<a class="button button__home-action js-no-boxes" href="#"><i class="fa fa-gift"></i>L'offrir</a>
+				</div>
+				<div class="grid-3 push-3 grid-11@xs grid-centered@xs push-0@xs">
+					<a class="button button__home-action js-no-boxes" href="#"><i class="fa fa-shopping-cart"></i> La recevoir</a>
+				</div>
+			</div>
+		@endif
+</div>
+
+<div class="container grid-11@xs gr-centered@xs">
 	{{-- Section how it works --}}
 	<div id="how-to" class="section">
 		<h1 class="section__title">Comment ça marche ?</h1>
@@ -67,7 +71,7 @@
 
 <div class="container-static">
 	<div class="row">
-		<div class="grid-4">
+		<div class="grid-4 grid-11@xs gr-centered@xs">
 			<div class="step">
 				<h2 class="step__title">Etape 1</h2>
 				<div class="step__picture-container">
@@ -78,7 +82,7 @@
 				</p>
 			</div>
 		</div>
-		<div class="grid-4">
+		<div class="grid-4 grid-11@xs gr-centered@xs">
 			<div class="step">
 				<h2 class="step__title">Etape 2</h2>
 				<div class="step__picture-container">
@@ -89,7 +93,7 @@
 				</p>
 			</div>
 		</div>
-		<div class="grid-4">
+		<div class="grid-4 grid-11@xs gr-centered@xs">
 			<div class="step">
 				<h2 class="step__title">Etape 3</h2>
 				<div class="step__picture-container">
@@ -106,14 +110,14 @@
 
 <div class="container">
 	{{-- Section Inside the box --}}
-	<div id="inside" class="section">
+	<div id="inside" class="section hide@xs">
 		<h1 class="section__title">Ce qu'il y a dans la boîte !</h1>
 	</div>
 </div>
 
 <div class="+spacer-small"></div>
 
-<div class="container-fluid">
+<div class="container-fluid hide@xs">
 	<div class="row">
 		<div class="grid-4 background background__green">
 			<div class="product">
@@ -165,9 +169,9 @@
 
 <div class="container-static">
 	@foreach ($articles->chunk(4) as $chunk)
-		<div class="row">
+		<div class="row row-align-center@xs">
 			@foreach ($chunk as $article)
-				<div class="grid-3">
+				<div class="grid-3 grid-12@xs gr-centered@xs">
 					<div class="partner">
 						<div class="partner__picture-container">
 							<a href="{{ action('MasterBox\Guest\BlogController@getArticle', ['slug' => $article->slug]) }}">
@@ -180,7 +184,7 @@
 		</div>
 	@endforeach
 
-	<div class="grid-5 grid-centered">
+	<div class="grid-5 grid-centered grid-11@xs">
 		<a class="button button__home-partner" href="{{ action('MasterBox\Guest\BlogController@getIndex') }}">Voir les autres boutiques complices ...</a>
 	</div>
 </div>
@@ -190,13 +194,13 @@
 
 
 {{-- Section seen --}}
-<div class="container">
+<div class="container hide@xs">
 	<div class="section">
 		<h1 class="section__title">Ils parlent de nous</h1>
 	</div>
 </div>
 
-	<div class="seen seen__wrapper">
+	<div class="seen seen__wrapper hide@xs">
 		<div class="container-static">
 			<div class="row seen__line">
 				<div class="grid-2">
