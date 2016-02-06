895f30ddd69bd8a6ba8ff5e758179bf41fd345ad
Loschcode <bonjour@laurentschaffner.com>
2016-02-06 19:19:36 +0100

    duplicate control order payment

--- website/app/Models/OrderPayment.php
+++ website/app/Models/OrderPayment.php
@@ -11,4 +11,36 @@ class OrderPayment extends Model {
    */
   protected $table = 'order_payments';
 
+
+  /**
+   * Create / Update
+   */
+  public static function boot()
+  {
+
+    parent::boot();
+
+    static::created(function($order_payment)
+    {
+
+      /**
+       * If it's a duplicate we don't add it
+       */
+      if (App\Models\OrderPayment::where('payment_id', '=', $order_payment->payment_id)
+                                 ->where('order_id', '=', $order_payment->order_id)
+                                 ->first() !== NULL) {
+
+        $order_payment->delete();
+
+      }
+
+    });
+
+    static::updating(function($order_payment)
+    {
+
+    });
+
+  }
+
 }
