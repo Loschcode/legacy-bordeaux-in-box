4dfb54a74e4c797b9055ef3458ac089c2565a8d6
Jeremie Ges <ges.jeremie@gmail.com>
2016-02-06 16:57:27 +0100

    tweak handler errors

--- website/app/Exceptions/Handler.php
+++ website/app/Exceptions/Handler.php
@@ -34,11 +34,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
-        /*
         if (app()->environment() == 'production')
         {
             $email = 'laurent@bordeauxinbox.com';
-            $data = array('exception' => $e, 'url' => url(''));
+            $data = array('exception' => $e, 'url' => request()->url());
 
             Mail::send('shared.emails.errors', $data, function($message) use ($email)
             {
@@ -46,8 +45,7 @@ class Handler extends ExceptionHandler
             });
 
         }
-        */
-        
+
 
         parent::report($e);
         
