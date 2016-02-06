3beea0ba5e6fc925ceb862decad0224cf3d75ba9
Loschcode <bonjour@laurentschaffner.com>
2016-02-06 11:15:10 +0100

    change migration

--- website/database/migrations/2016_01_10_001820_change_user_and_related_to_customer.php
+++ website/database/migrations/2016_01_10_001820_change_user_and_related_to_customer.php
@@ -14,6 +14,44 @@ class ChangeUserAndRelatedToCustomer extends Migration
     public function up()
     {
 
+      // MOVED
+      Schema::table('box_answers', function ($table) {
+        $table->dropForeign('box_answers_box_question_id_foreign');
+      });
+
+      Schema::table('box_question_customer_answers', function ($table) {
+        $table->dropForeign('user_answers_box_question_id_foreign');
+        $table->dropForeign('user_answers_user_profile_id_foreign');
+      });
+
+      Schema::table('customer_payment_profiles', function ($table) {
+        $table->dropForeign('user_payment_profiles_user_profile_id_foreign');
+      });
+
+      Schema::table('customer_profiles', function ($table) {
+        $table->dropForeign('user_profiles_user_id_foreign');
+      });
+
+      Schema::table('order_billings', function ($table) {
+        $table->dropForeign('order_billings_order_id_foreign');
+      });
+
+      Schema::table('order_destinations', function ($table) {
+        $table->dropForeign('order_destinations_order_id_foreign');
+      });
+
+      Schema::table('orders', function ($table) {
+        $table->dropForeign('orders_delivery_serie_id_foreign');
+        $table->dropForeign('orders_user_id_foreign');
+        $table->dropForeign('orders_user_profile_id_foreign');
+      });
+
+      Schema::table('payments', function ($table) {
+        $table->dropForeign('payments_user_id_foreign');
+        $table->dropForeign('payments_user_profile_id_foreign');
+      });
+      // END OF MOVED
+
       /**
        * Workaround for the ENUM problem (Thank you Laravel 5.2, you suck.)
        */
@@ -25,7 +63,7 @@ class ChangeUserAndRelatedToCustomer extends Migration
        * On the User and related to Customer
        * 
        */
-      
+
       // Table : users
       Schema::rename('users', 'customers');
 
