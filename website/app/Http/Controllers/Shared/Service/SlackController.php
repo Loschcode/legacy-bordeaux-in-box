ecbcf571eea67d961037866c2c385c9e7cd9ea0d
Jeremie Ges <ges.jeremie@gmail.com>
2016-02-11 06:32:25 +0100

    slack command marketing in progress

--- website/app/Http/Controllers/Shared/Service/SlackController.php
+++ website/app/Http/Controllers/Shared/Service/SlackController.php
@@ -17,12 +17,32 @@ class SlackController extends BaseController {
 
   public function postAddTask()
   {
-    dd(request()->all());
 
-    $trello = new Trello();
+    $command = request()->input('command');
+    $text = trim(request()->input('text'));
 
-    $trello->addTask('test', 'List', 'Je suis enorme putain', 'Superbe description');
+    /**
+     * Marketing command
+     */
+    if ($command === '/marketing') {
 
+      // Explode params
+      $params = explode(' ', $text);
+
+      // Fetch username
+      $username = $params[0];
+        
+      // Forget username
+      unset($params[0]);
+
+      // Fetch task description
+      $task = implode(' ', $params);
+
+      // Add task
+      $trello = new Trello();
+      $response = $trello->addTask('test', 'List', $task);
+
+    }
 
   }
 
