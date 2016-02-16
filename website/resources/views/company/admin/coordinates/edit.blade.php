7612918089ed59a97c1b547cb240047401f8b8fd
Jeremie Ges <ges.jeremie@gmail.com>
2016-02-16 22:39:53 +0100

    edit coordinates design

--- website/resources/views/company/admin/coordinates/edit.blade.php
+++ website/resources/views/company/admin/coordinates/edit.blade.php
@@ -1,84 +1,42 @@
-@extends('masterbox.layouts.admin')
-
-@section('page')
-  <i class="fa fa-map-marker"></i> Edition coordonnées #{{$coordinate->id}}
-@stop
-
-@section('buttons')
-
-@if (URL::previous() != Request::root())
-  
-  <a href="{{URL::previous()}}" class="spyro-btn spyro-btn-success">Retour</a>
-
-@endif
-
-@stop
+@extends('company.layouts.admin')
 
 @section('content')
+
+  <div class="row">
+    <div class="grid-8">
+      <h1 class="title title__section"><i class="fa fa-location-arrow"></i> Edition Coordonnée #{{ $coordinate->id }}</h1>
+    </div>
+    <div class="grid-4 +text-right">
+      <a href="{{ action('Company\Admin\CoordinatesController@getIndex') }}" class="button button__section"><i class="fa fa-list"></i> Voir toutes les coordonnées</a>
+    </div>
+  </div>
+
+  <div class="divider divider__section"></div>
+
   {!! Form::open() !!}
   {!! Form::hidden('coordinate_id', $coordinate->id) !!}
-
-  <strong>Google Place ID : {{$coordinate->place_id}}</strong>
-  <br />
-  <br />
-
-  <!-- Address -->
-  <div class="form-group @if ($errors->first('address')) has-error has-feedback @endif">
-    {!! Form::label("address", "Adresse", ['class' => 'control-label']) !!}
-    {!! Form::text("address", ($coordinate->address) ? $coordinate->address : Request::old("address"), ['class' => 'form-control']) !!}
-
-    @if ($errors->first('address'))
-      <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-      <span class="help-block">{{ $errors->first('address') }}</span>
-    @endif
+  
+  <div class="typography">
+    <strong>Google Place ID:</strong> {{$coordinate->place_id}}
   </div>
 
-  <!-- Address detail -->
-  <div class="form-group @if ($errors->first('address_detail')) has-error has-feedback @endif">
-    {!! Form::label("address_detail", "Complément d'adresse", ['class' => 'control-label']) !!}
-    {!! Form::textarea("address_detail", ($coordinate->address_detail) ? $coordinate->address_detail : Request::old("address_detail"), ['class' => 'form-control']) !!}
+  {!! Form::label("address", "Adresse", ['class' => 'form__label']) !!}
+  {!! Form::text("address", ($coordinate->address) ? $coordinate->address : Request::old("address"), ['class' => 'form__input']) !!}
 
-    @if ($errors->first('address_detail'))
-      <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-      <span class="help-block">{{ $errors->first('address_detail') }}</span>
-    @endif
-  </div>
+  {!! Form::label("address_detail", "Complément d'adresse", ['class' => 'form__label']) !!}
+  {!! Form::text("address_detail", ($coordinate->address_detail) ? $coordinate->address_detail : Request::old("address_detail"), ['class' => 'form__input']) !!}
 
-  <!-- City -->
-  <div class="form-group @if ($errors->first('city')) has-error has-feedback @endif">
-    {!! Form::label("city", "Ville", ['class' => 'control-label']) !!}
-    {!! Form::text("city", ($coordinate->city) ? $coordinate->city : Request::old("city"), ['class' => 'form-control']) !!}
+  {!! Form::label("city", "Ville", ['class' => 'form__label']) !!}
+  {!! Form::text("city", ($coordinate->city) ? $coordinate->city : Request::old("city"), ['class' => 'form__input']) !!}
 
-    @if ($errors->first('city'))
-      <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-      <span class="help-block">{{ $errors->first('city') }}</span>
-    @endif
-  </div>
+  {!! Form::label("zip", "Code postal", ['class' => 'form__label']) !!}
+  {!! Form::text("zip", ($coordinate->zip) ? $coordinate->zip : Request::old("zip"), ['class' => 'form__input']) !!}
 
-  <!-- Zip -->
-  <div class="form-group @if ($errors->first('zip')) has-error has-feedback @endif">
-    {!! Form::label("zip", "Code postal", ['class' => 'control-label']) !!}
-    {!! Form::text("zip", ($coordinate->zip) ? $coordinate->zip : Request::old("zip"), ['class' => 'form-control']) !!}
+  {!! Form::label("country", "Pays", ['class' => 'form__label']) !!}
+  {!! Form::text("country", ($coordinate->country) ? $coordinate->country : Request::old("country"), ['class' => 'form__input']) !!}
+  
+  <div class="+spacer-extra-small"></div>
 
-    @if ($errors->first('zip'))
-      <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-      <span class="help-block">{{ $errors->first('zip') }}</span>
-    @endif
-  </div>
-
-  </div>
-
-  <!-- Country -->
-  <div class="form-group @if ($errors->first('country')) has-error has-feedback @endif">
-    {!! Form::label("country", "Pays", ['class' => 'control-label']) !!}
-    {!! Form::text("country", ($coordinate->country) ? $coordinate->country : Request::old("country"), ['class' => 'form-control']) !!}
-
-    @if ($errors->first('country'))
-      <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-      <span class="help-block">{{ $errors->first('country') }}</span>
-    @endif
-  </div>
-
-  {!! Form::submit("Sauvegarder les modifications", ['class' => 'spyro-btn spyro-btn-lg spyro-btn-success']) !!}
+  {!! Form::submit("Sauvegarder les modifications", ['class' => 'button button__default --blue']) !!}
 
 @stop
