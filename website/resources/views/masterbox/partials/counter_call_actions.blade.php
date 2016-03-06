e3d7ac9f9c434dbab584b1c3795e5e717f28dd8b
Jeremie Ges <ges.jeremie@gmail.com>
2016-03-06 16:58:53 +0100

    tweaks home page

--- website/resources/views/masterbox/partials/counter_call_actions.blade.php
+++ website/resources/views/masterbox/partials/counter_call_actions.blade.php
@@ -4,7 +4,7 @@
 <div class="grid-11@xs gr-centered@xs">
   <div class="counter">
     <div class="counter__content">
-      Il reste moins de 10 boxs et {{ str_replace('dans', '', strtolower(Html::diffHumans($next_series->first()->delivery, 5
+      Il reste moins de 5 boxs et {{ str_replace('dans', '', strtolower(Html::diffHumans($next_series->first()->delivery, 5
       ))) }} pour commander la box de {!! Html::convertMonth($next_series->first()->delivery) !!}
     </div>
   </div>
