cffe56ca6cc7a940d867dccce768a0ca29f4850e
Jeremie Ges <ges.jeremie@gmail.com>
2016-03-17 03:41:03 +0100

    responsive menu

--- website/resources/views/masterbox/partials/navbar.blade.php
+++ website/resources/views/masterbox/partials/navbar.blade.php
@@ -1,3 +1,4 @@
+
   <div class="header">
     @if (Auth::guard('customer')->check())
       <div class="header__account">
@@ -14,7 +15,17 @@
       </a>
     </div>
     <div class="header__navbar-container">
-      <ul class="header__navbar">
+      <ul class="header__navbar js-menu">
+        <div class="show@xs hide">
+          @if (Auth::guard('customer')->check())
+            <li><a href="{{ action('MasterBox\Customer\ProfileController@getIndex') }}" class="header__account-link"><i class="fa fa-user"></i> Mon compte</a>
+            </li>
+          @else
+            <li>
+              <a href="{{ action('MasterBox\Connect\CustomerController@getIndex') }}" class="header__account-link"><i class="fa fa-lock"></i> Connexion</a>
+            </li>
+          @endif
+        </div>
         <li><a href="{{ action('MasterBox\Guest\HomeController@getConcept') }}" class="header__navbar-link">Concept</a></li>
         @if (App\Models\DeliverySerie::nextOpenSeries()->first() !== NULL)
           @if (App\Models\DeliverySerie::nextOpenSeries()->first()->getCounter() !== 0 || App\Models\DeliverySerie::nextOpenSeries()->first()->getCounter() === FALSE)
@@ -31,6 +42,7 @@
 
         <li><a href="{{ action('MasterBox\Guest\HomeController@getLastBoxs') }}" class="header__navbar-link">Dernières Boxs</a></li>
         <li><a href="{{ action('MasterBox\Guest\BlogController@getIndex') }}" class="header__navbar-link">Complices</a></li>
+
       </ul>
     </div>
   </div>
