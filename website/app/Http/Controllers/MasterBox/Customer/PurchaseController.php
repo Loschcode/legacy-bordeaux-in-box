f6999da6956c07693d02a983d16f18d0b4424910
Loschcode <bonjour@laurentschaffner.com>
2016-03-26 05:03:20 +0100

    bug fix

--- website/app/Http/Controllers/MasterBox/Customer/PurchaseController.php
+++ website/app/Http/Controllers/MasterBox/Customer/PurchaseController.php
@@ -594,9 +594,7 @@ class PurchaseController extends BaseController {
 
       if ($coupon === 'FIN-MARS-2016') {
 
-        dd($order_preference);
-
-        if ($order_preference->frequency === 1) {
+        if ($order_preference->frequency == 1) {
         
           $order_preference->unity_price = 21.90;
           $order_building->delivery_serie_id = 37; // RAW
